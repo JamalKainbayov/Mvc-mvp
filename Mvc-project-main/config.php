e46efc1ef2cc925e423d5b6fb39505bd80755e13
Duckie <Jamalkainbayov2005@gmail.com>
2025-06-26 12:47:04 +0200

    edits

--- Mvc-project-main/config.php
+++ Mvc-project-main/config.php
@@ -1,5 +1,4 @@
 <?php
-// config.php
 
 $host = 'localhost';
 $dbname = 'mvc_project';
@@ -7,7 +6,6 @@ $username = 'root';
 $password = '';
 
 try {
-    // verbinding met database maken
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
